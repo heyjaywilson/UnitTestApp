9b635aad4b32ad3162375176b1d07b7faf30e79f
Maegan <maegan@maeganwilson.com>
2021-09-14 06:30:53 -0500

    Initial Commit

---
+++ UnitTestApp/UnitTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestAppApp.swift
+//  UnitTestApp
+//
+//  Created by Maegan Wilson on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
